afbdf40fc335d3804c1b2f69d1c2e89bdc12e197
Sam Goto <samuelgoto@gmail.com>
2017-06-08 15:40:29 -0700

    adding examples of usage

--- prolog.js
+++ prolog.js
@@ -27,6 +27,9 @@ class Term {
     this.functor = functor;
     this.args = args || [];
   }
+  static of(functor, args) {
+    return new Term(functor, args);
+  }
   match(other) {
     if (other instanceof Term) {
       if (this.functor !== other.functor) {
@@ -35,24 +38,22 @@ class Term {
       if (this.args.length !== other.args.length) {
         return null;
       }
-      return zip([this.args, other.args]).map(function(args) {
+      return Term.zip([this.args, other.args]).map(function(args) {
         return args[0].match(args[1]);
-      }).reduce(this.mergeBindings, new Map());
+      }).reduce(Term.mergeBindings, new Map());
     }
     return other.match(this);
   };
 
-  substitute(bindings) {
-    return new Term(this.functor, this.args.map(function(arg) {
-      return arg.substitute(bindings);
-    }));
-  };
+  static zip(arrays) {
+    return arrays[0].map(function(element, index) {
+      return arrays.map(function(array) {
+	return array[index];
+      });
+    });
+  }
 
-  *query(database) {
-    yield* database.query(this);
-  };
-
-  mergeBindings(bindings1, bindings2) {
+  static mergeBindings(bindings1, bindings2) {
     if (!bindings1 || !bindings2) {
       return null;
     }
@@ -81,27 +82,30 @@ class Term {
     }
     return bindings;
   };
+
+  substitute(bindings) {
+    return new Term(this.functor, this.args.map(function(arg) {
+      return arg.substitute(bindings);
+    }));
+  };
+
+  *query(database) {
+    yield* database.query(this);
+  };
 }
 
-Term.TRUE = new Term('true');
-
-Term.TRUE.substitute = function() {
+class True extends Term {
+  constructor() {
+    super("true");
+  }
+  substitute() {
     return this;
-};
-
-Term.TRUE.query = function*() {
+  };
+  *query() {
     yield this;
-};
-
-function zip(arrays) {
-    return arrays[0].map(function(element, index) {
-        return arrays.map(function(array) {
-            return array[index];
-        });
-    });
-
-
+  };
 }
+
 
 class Rule {
   constructor(head, body) {
@@ -112,7 +116,7 @@ class Rule {
 
 class Conjunction extends Term {
   constructor(args) {
-    this.args = args;
+    super(undefined, args);
   }
 
   *query(database) {
@@ -123,7 +127,7 @@ class Conjunction extends Term {
         yield self.substitute(bindings);
       } else {
         for (var item of database.query(arg.substitute(bindings))) {
-          var unified = mergeBindings(arg.match(item), bindings);
+          var unified = Term.mergeBindings(arg.match(item), bindings);
           if (unified) {
             yield* solutions(index + 1, unified);
           }
@@ -146,6 +150,7 @@ class Database {
   }
   *query(goal) {
     for (var i = 0, rule; rule = this.rules[i]; i++) {
+      // console.log("hi");
       var match = rule.head.match(goal);
       if (match) {
         var head = rule.head.substitute(match);
@@ -158,19 +163,68 @@ class Database {
   }
 }
 
-var knownTerm = new Term('father_child', [
-    new Term('eric'), new Term('thorne')
+//var knownTerm = Term.of('father_child', [Term.of('eric'), Term.of('thorne')]);
+//var goal = Term.of('father_child', [Term.of('eric'), Term.of('thorne')]);
+//var bindings = goal.match(knownTerm);
+//console.log(bindings);
+
+// var goal = Term.of("person", [Term.of("Hillary")]);
+// var bindings = goal.match(Term.of("person", [Term.of("Hillary")]));
+// console.log(bindings);
+
+var X = new Variable("X");
+var Y = new Variable("Y");
+var Z = new Variable("Z");
+
+var db = new Database([
+  new Rule(Term.of("father", [Term.of("Hugh"), Term.of("Hillary")]), new True()),
+
+  new Rule(Term.of("mother", [Term.of("Hillary"), Term.of("Chelsea")]), new True()),
+  new Rule(Term.of("father", [Term.of("Bill"), Term.of("Chelsea")]), new True()),
+
+  new Rule(Term.of("father", [Term.of("Bill"), Term.of("Bill's fake child")]), new True()),
+  // if x is a mother of y, x is a parent of y
+  new Rule(Term.of("parent", [X, Y]), Term.of("mother", [X, Y])),
+  // if x is a father of y, x is a parent of y
+  new Rule(Term.of("parent", [X, Y]), Term.of("father", [X, Y])),
+  // if z is a parent of x and z is a parent of y, x and y are siblings
+  new Rule(Term.of("sibling", [X, Y]), new Conjunction([Term.of("parent", [Z, X], Term.of("parent", [Z, Y]))])),
+  // if x is a parent of y, x is an ancestor of y
+  new Rule(Term.of("ancestor", [X, Y]), Term.of("parent", [X, Y])),
+  // if x is a parent of z and z is an ancestor of y, x is an ancestor of y
+  new Rule(Term.of("ancestor", [X, Y]), new Conjunction([Term.of("parent", [X, Z], Term.of("ancestor", [Z, Y]))])),
 ]);
 
+for (mother of db.query(Term.of("mother", [Term.of("Hillary"), new Variable("X")]))) {
+  // console.log(mother);
+}
+
+for (parent of db.query(Term.of("parent", [new Variable("X"), Term.of("Chelsea")]))) {
+  // console.log(parent);
+}
+
+for (sibling of db.query(Term.of("sibling", [new Variable("X"), Term.of("Chelsea")]))) {
+  // console.log(sibling);
+}
+
+for (ancestor of db.query(Term.of("ancestor", [new Variable("X"), Term.of("Chelsea")]))) {
+  console.log(ancestor);
+}
+
+
+return;
+
+var knownTerm = Term.of('father_child', [Term.of('eric'), Term.of('thorne')]);
 var x = new Variable('X');
-
-var goal = new Term('father_child', [
-    new Term('eric'), x
-]);
-
+var goal = Term.of('father_child', [Term.of('eric'), x]);
 var bindings = goal.match(knownTerm);
 
 console.log(bindings);
 
 var value = goal.substitute(bindings);
 console.log(`Goal with substituted variables: ${JSON.stringify(value)}`);
+
+var goal = Term.of("father_child", [new Variable("Y"), Term.of("thorne")]);
+var bindings = goal.match(knownTerm);
+console.log(bindings);
+console.log(`Thorne's dad is: ${JSON.stringify(goal.substitute(bindings))}`);
